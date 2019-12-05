d6d665d49ae3bc3b9f14fcf7974f7d3b36ed980d
Ricky-yu <ts00484515@gmail.com>
2019-12-05 22:23:59 +0900

    Initial Commit

---
+++ CountApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CountApp
+//
+//  Created by CHEN SINYU on 2019/12/04.
+//  Copyright © 2019 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var countUpButton: UIButton!
+    
+    @IBOutlet weak var countDownButton: UIButton!
+    
+    @IBOutlet weak var countResetButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    
+    private var viewModel: CounterRxViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViewModel()
+    }
+    private func setupViewModel() {
+        viewModel = CounterRxViewModel()
+        let input = CounterViewModelInput(
+            countUpButton: countUpButton.rx.tap.asObservable(),
+            countDownButton: countDownButton.rx.tap.asObservable(),
+            countResetButton: countResetButton.rx.tap.asObservable()
+        )
+        
+        viewModel.setup(input: input)
+        
+        viewModel.outputs?.counterText.drive(countLabel.rx.text).disposed(by: disposeBag)
+    }
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CountApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+   pod'RxSwift','~>4.3.1'
+   pod'RxCocoa','~>4.3.1'
+
+  # Pods for CountApp
+
+  target 'CountAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CountAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
